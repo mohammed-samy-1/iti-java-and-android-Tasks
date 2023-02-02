c41374ec0a7f20016ceb69eb5e16b42240940f04
mo samy <medo.samy.0123@mail.com>
2023-02-02 10:27:55 +0200

    longest name write a function to find the longest name in an array and print the name and its index

--- src/LongestNameAndIndex.java
+++ src/LongestNameAndIndex.java
@@ -1,3 +1,17 @@
 public class LongestNameAndIndex {
+    public static void main(String[] args) {
+        String[] names = new String[]{"mohammed", "samy", "ahmed", "Abd Al-rahman" , "mahmoud", "mostafa"};
+        int longestNameIndex = longestName(names);
+        System.out.println("longest name is : " + names[longestNameIndex]
+                + "\nin index : " + longestNameIndex);
+    }
 
+    private static int longestName(String[] names) {
+        int index = 0;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() > names[index].length())
+                index = i;
+        }
+        return index;
+    }
 }
